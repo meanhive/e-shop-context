503a5bd3d17a94c8516d354efa4e88af7c252c6d
meanhive <meanhive4@gmail.com>
2023-01-23 11:51:46 +0530

    my first commit

---
+++ src/API/ProductApi.js
@@ -0,0 +1,61 @@
+import { useState, useMemo, useEffect } from 'react'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+const url = `https://dummyjson.com`
+
+function useProductApi() {
+    const [products,setProducts]  = useState([])
+    const [cart,setCart] = useState([])
+
+    // states to calculation of cart subtotal,total,tax,delivery charges
+    const [subTotal, setSubTotal] = useState(0)
+    const [discount,setDiscount]  = useState(0)
+    const [gst,setGst] = useState(5)
+    const [dc,setDc] = useState(50)
+
+    const readProducts = async () => {
+        const out = await axios.get(`${url}/products`)
+            setProducts(out.data.products)
+    }
+
+    const initValue = useMemo(() => (
+        { value: [products,setProducts]}
+    ))
+
+    useEffect(() => {
+       readProducts()
+    },[])
+
+    // add product to cart
+    const addToCart = async (data) => {
+      // console.log('cart =', product)
+      
+      // product exist in cart or not
+      const check = cart.every(item => {
+        return item.id !== data.id;
+      });
+
+      // console.log('check =', check);
+
+        if(check) {
+          toast.success('product added to cart');
+          setCart([...cart, { ...data, quantity: 1 }])
+        } else {
+          toast.warning('Product already in cart.')
+        }
+
+    }
+
+  return {
+    products: initValue,
+    cart: [cart,setCart],
+    addToCart: addToCart,
+    subTotal: [subTotal,setSubTotal],
+    gst: [gst,setGst],
+    dc: [dc,setDc],
+    discount: [discount,setDiscount]
+  }
+}
+
+export default useProductApi
